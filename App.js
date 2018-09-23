8630d968380a9731f8cffe25edd8c83be2ae80cf
Jacob Rosenthal <jacobrosenthal@gmail.com>
2018-09-22 21:04:24 -0700

    deep alias

--- App.js
+++ App.js
@@ -8,6 +8,8 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import { values } from 'test';
+import { map } from 'underscore';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -22,6 +24,8 @@ export default class App extends Component<Props> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>{values.map((i) => i + 1)}</Text>
+        <Text style={styles.welcome}>{map(values, (i) => i + 1)}</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
